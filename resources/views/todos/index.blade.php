8cacc1b01371bcedd59d262128fd422557470853
Fabrizio Dalia <et24.daliafabrizio@gmail.com>
2024-05-29 15:42:10 -0300

    Seguimos con el curso

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -2,5 +2,13 @@
 
 <!-- Acá hacemos referencia a la sección 'content' del archivo 'app' que definimos arriba  -->
 @section('content')
-    Hola Mundo
+    <div class="container w-25 border p-4 mt-4">
+        <form>
+            <div class="mb-3">
+                <label for="title" class="form-label">Título de la tarea</label>
+                <input type="text" class="form-control" name="title">
+            </div>            
+            <button type="submit" class="btn btn-primary">Crear nueva tarea</button>
+        </form>
+    </div>
 @endsection
